7889a0ce844ae0a42589e597fb11c016dd27a4f3
Jakhongir <zhaxongir.sirozhiddinov@gmail.com>
2024-01-17 05:22:16 +0500

    feat: add solution for 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,33 @@
+import { createWriteStream } from 'node:fs';
+import path from 'node:path';
+
+const { dirname } = import.meta;
+const { stdout, stdin, stderr, exit } = process;
+
+const writeFile = ({ fileName, readStream = stdin }) => {
+  const pathToFile = path.resolve(dirname, fileName);
+
+  const writeStream = createWriteStream(pathToFile, { encoding: 'utf-8' });
+
+  stdout.write('Hello!\n');
+
+  readStream.on('data', (data) => {
+    const input = data.toString().trim();
+
+    if (input === 'exit' || input === 'EXIT') {
+      exit();
+    }
+
+    writeStream.write(data);
+  });
+
+  readStream.on('error', (error) => {
+    stderr.write(error);
+  });
+
+  process.on('SIGINT', () => exit());
+
+  process.on('exit', () => stdout.write('Goodbye!\n'));
+};
+
+writeFile({ fileName: 'user-input.txt' });
