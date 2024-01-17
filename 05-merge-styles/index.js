b0e6b071a002ec4defdc159e8e5cabfd33e559a2
Jakhongir <zhaxongir.sirozhiddinov@gmail.com>
2024-01-17 08:13:53 +0500

    feat: add solution for 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,38 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { readdir } from 'node:fs/promises';
+import path from 'node:path';
+
+const { dirname } = import.meta;
+const { stderr } = process;
+
+const mergeStyles = async ({
+  srcDir = 'styles',
+  destDir = 'project-dist',
+  bundleFileName = 'bundle.css',
+} = {}) => {
+  try {
+    const pathToSrcDir = path.resolve(dirname, srcDir);
+    const pathToDestDir = path.resolve(dirname, destDir);
+    const pathToBundleFile = path.resolve(pathToDestDir, bundleFileName);
+
+    const writeStream = createWriteStream(pathToBundleFile, {
+      encoding: 'utf-8',
+    });
+
+    const dirents = await readdir(pathToSrcDir, { withFileTypes: true });
+
+    const cssFiles = dirents.filter(
+      (dirent) => dirent.isFile() && dirent.name.endsWith('.css')
+    );
+
+    for await (const file of cssFiles) {
+      const pathToFile = path.resolve(pathToSrcDir, file.name);
+      const readStream = createReadStream(pathToFile, { encoding: 'utf-8' });
+      readStream.pipe(writeStream);
+    }
+  } catch (error) {
+    stderr.write(`${error}\n`);
+  }
+};
+
+mergeStyles();
